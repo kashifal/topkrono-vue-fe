7541686a4b784dc15e7b27114c01e37c5ef75202
Kashif Sulehria <captainkashi4@gmail.com>
2023-06-07 23:53:15 +0500

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-primary text-secondary text-7xl">
+        IndusAppps 
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#224386',
+        secondary:'#f28730'
+      },
+    },
+  },
+  plugins: [],
+}
