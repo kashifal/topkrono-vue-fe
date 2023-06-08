8899329495001bae91b0159cb004dda84ccb5907
tayyabaimam <tayyaba.imam@gmail.com>
2023-06-09 00:04:19 +0500

    plumbing

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,177 @@
 <template>
-    <div class="bg-primary text-secondary text-7xl">
-        IndusAppps 
-    </div>
+  <!-- <div class="bg-primary text-secondary text-7xl">IndusAppps</div>-->
+  <!-- Navbar -->
+  <header class="bg-white">
+    <nav
+      class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+      aria-label="Global"
+    >
+      <div class="flex items-center gap-x-12">
+        <a href="#" class="-m-1.5 p-1.5">
+          <img class="h-[5rem]" src="../assets/logo.png" alt="" />
+        </a>
+        <div class="hidden lg:flex lg:gap-x-12">
+          <a class="text-medium leading-6 text-gray-900">Home</a>
+          <Menu as="div" class="relative inline-block text-left">
+            <div>
+              <MenuButton
+                class="inline-flex w-full justify-center gap-x-1.5 border-none bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              >
+                Services
+                <ChevronDownIcon
+                  class="-mr-1 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </MenuButton>
+            </div>
+
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
+              <MenuItems
+                class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a
+                      href="/plumbing"
+                      :class="[
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm',
+                      ]"
+                      >Plumbing</a
+                    >
+                  </MenuItem>
+                </div>
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a
+                      href="#"
+                      :class="[
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm',
+                      ]"
+                      >Archive</a
+                    >
+                  </MenuItem>
+                </div>
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a
+                      href="#"
+                      :class="[
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm',
+                      ]"
+                      >Share</a
+                    >
+                  </MenuItem>
+                </div>
+                <div class="py-1">
+                  <MenuItem v-slot="{ active }">
+                    <a
+                      href="#"
+                      :class="[
+                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                        'block px-4 py-2 text-sm',
+                      ]"
+                      >Delete</a
+                    >
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </transition>
+          </Menu>
+          <a class="text-medium leading-6 text-gray-900">About Us</a>
+          <a class="text-medium leading-6 text-gray-900">Service Area</a>
+        </div>
+      </div>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true"
+        >
+          <span class="sr-only">Open main menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div class="hidden lg:flex">
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900"
+          >Login</a
+        >
+      </div>
+    </nav>
+    <Dialog
+      as="div"
+      class="lg:hidden"
+      @close="mobileMenuOpen = false"
+      :open="mobileMenuOpen"
+    >
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center justify-between">
+          <a href="#" class="-m-1.5 p-1.5">
+            <img class="h-15 w-auto" src="../assets/logo.png" alt="" />
+          </a>
+          <button
+            type="button"
+            class="-m-2.5 rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = false"
+          >
+            <span class="sr-only">Close menu</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >{{ item.name }}</a
+              >
+            </div>
+            <div class="py-6">
+              <a
+                href="#"
+                class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >Log in</a
+              >
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
 </template>
 
-<script setup>
+<!--hero section -->
 
+<script setup>
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import { ChevronDownIcon } from "@heroicons/vue/20/solid";
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  { name: "Home", href: "#" },
+  { name: "Services", href: "#" },
+  { name: "About Us", href: "#" },
+  { name: "Service Area", href: "#" },
+];
+
+const mobileMenuOpen = ref(false);
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomePage from '@/pages/HomePage.vue';
- 
+import HomePage from "@/pages/HomePage.vue";
+import Plumbing from "@/pages/Plumbing.vue";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomePage
-        }  
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomePage,
+    },
+    {
+      path: "/plumbing",
+      name: "plumbing",
+      component: Plumbing,
+    },
+  ],
+});
 
 export default router;
