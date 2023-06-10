7b8b422c5cc30acc66f5d6a1e75e967843a38aaa
Kashif Sulehria <captainkashi4@gmail.com>
2023-06-10 16:01:07 +0500

    design / form

---
+++ src/pages/PlumbingIndex.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Service />
+    <AdditionalInfo />
+  </div>
+</template>
+
+<script setup>
+import Service from "@/components/Forms/PlumbingForms/Service.vue";
+import AdditionalInfo from "@/components/Forms/PlumbingForms/AdditionalInfo.vue";
+</script>
+
+<style lang="scss" scoped></style>
